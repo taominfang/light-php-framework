cdf280ea18484a981f1bb10fd7677a4599b1d49d
taominfang@gmail.com <taominfang@gmail.com@aa78d8fb-4fae-0394-170d-2d2db8bb35a2>
2013-01-21 07:24:36 +0000

    add ajax decorator add phpunit example

--- trunk/system/basic_controller.php
+++ trunk/system/basic_controller.php
@@ -1,16 +1,17 @@
-<?php 
+<?php
 class BasicController{
-	
+
 	public $tpl_name="";
-	
+
 	public $decorator="";
-	
+
 	protected $required_in_session=array();
-	
+
 	public function pre_filter(&$methodName=null){
-		
-		
+
+
 		session_start();
+
 		header("Cache-Control: no-cache, must-revalidate");
 		//dd($this->required_in_session);
 		if(!empty($this->required_in_session)){
@@ -21,29 +22,29 @@ class BasicController{
 				}
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	public function post_filter(&$methodName=null){
-		
+
 	}
-	
+
 	public function set($pName,$pValue){
 		global $smarty;
 		$smarty->assign($pName,$pValue);
 	}
-	
+
 	public function setTpl($tplName){
 		$this->tpl_name=$tplName;
 	}
-	
+
 	public function reportErrorByAjax($em){
 		$this->decorator='ajax';
 		$this->setTpl('/general_error.tpl');
 		$this->set('errorMessage',$em);
 	}
-	
+
 	public function reportError($em	){
 		$this->setTpl('/general_error.tpl');
 		$this->set('errorMessage',$em);
